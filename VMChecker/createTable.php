feb3f693dbec6ea25832c182b7d3992285f0bdaa
LordLolzeye <almantud@yahoo.com>
2015-09-29 15:00:05 +0300

    Updated create table query.

--- VMChecker/createTable.php
+++ VMChecker/createTable.php
@@ -1,13 +1,13 @@
 <?php
 $db = new SQLite3('queue.db');
 $db->exec("CREATE TABLE IF NOT EXISTS `queue` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `date` varchar(100) NOT NULL,
-  `user` varchar(80) NOT NULL,
-  `email` varchar(80) NOT NULL,
-  `localDir` varchar(255) NOT NULL,
-  `status` varchar(30) NOT NULL DEFAULT 'inQueue',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `user` (`user`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;");
+`id` int(255) NOT NULL AUTO_INCREMENT,
+`date` varchar(100) NOT NULL,
+`user` varchar(80) NOT NULL,
+`email` varchar(80) NOT NULL,
+`localDir` varchar(255) NOT NULL,
+`status` varchar(30) NOT NULL DEFAULT 'inQueue',
+PRIMARY KEY (`id`),
+UNIQUE KEY `user` (`user`)
+);");
 ?>
