53fc5a7d89470fffaeca8b91b2c8ef2084d5cd51
LordLolzeye <almantud@yahoo.com>
2015-09-29 15:29:18 +0300

    Updated for SQLite

--- VMChecker/createTable.php
+++ VMChecker/createTable.php
@@ -1,13 +1,11 @@
 <?php
 $db = new SQLite3('queue.db');
 $db->exec("CREATE TABLE IF NOT EXISTS `queue` (
-`id` int(255) NOT NULL AUTO_INCREMENT,
+`id` INTEGER PRIMARY KEY AUTOINCREMENT,
 `date` varchar(100) NOT NULL,
 `user` varchar(80) NOT NULL,
 `email` varchar(80) NOT NULL,
 `localDir` varchar(255) NOT NULL,
-`status` varchar(30) NOT NULL DEFAULT 'inQueue',
-PRIMARY KEY (`id`),
-UNIQUE KEY `user` (`user`)
+`status` varchar(30) NOT NULL DEFAULT 'inQueue'
 );");
 ?>
